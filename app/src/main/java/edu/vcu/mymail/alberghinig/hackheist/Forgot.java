eded41e296369536ae087f40fc53465d37bf8350
Gavin Alberghini <33029288+gavinjalberghini@users.noreply.github.com>
2019-03-09 22:26:00 -0500

    Slight changes to forgot.java

--- app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Forgot.java
+++ app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Forgot.java
@@ -2,12 +2,10 @@ package edu.vcu.mymail.alberghinig.hackheist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class Forgot extends AppCompatActivity {
 
