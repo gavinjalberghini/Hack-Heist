c4b78402d8f09420ea7b1fef526669bb0ec91113
Sinead Foley <smfoley2nd@gmail.com>
2019-03-12 02:47:40 -0400

    Added to devinfo and help screens

--- app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Review.java
+++ app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Review.java
@@ -3,8 +3,14 @@ package edu.vcu.mymail.alberghinig.hackheist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class Review extends AppCompatActivity {
 
@@ -13,8 +19,18 @@ public class Review extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
 
-        final ImageButton backButton = findViewById(R.id.Review_BackButton);
+        final RatingBar ratingBar = findViewById(R.id.ratingBar);
+        //final int ratingNumber = 0;//ratingBar.getNumStars();
 
+        final EditText reviewAnswer = findViewById(R.id.question_for_dev);
+        reviewAnswer.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        reviewAnswer.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        final ImageButton backButton = findViewById(R.id.Review_BackButton);
+        final Button submitButton = findViewById(R.id.submit_button);
+
+        final Toast successPopUp = Toast.makeText(getApplicationContext(),
+                "Thank You!",
+                Toast.LENGTH_LONG);
 
         View.OnClickListener goBackEvent = new View.OnClickListener() {
             @Override
@@ -24,6 +40,25 @@ public class Review extends AppCompatActivity {
             }
         };
 
+        View.OnClickListener clearScreen = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reviewAnswer.getText().clear();
+                ratingBar.setRating(0);
+                successPopUp.show();
+            }
+        };
+
+
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                //ratingNumber = ratingBar.getNumStars();
+            }
+        });
+
         backButton.setOnClickListener(goBackEvent);
+        submitButton.setOnClickListener(clearScreen);
     }
 }
