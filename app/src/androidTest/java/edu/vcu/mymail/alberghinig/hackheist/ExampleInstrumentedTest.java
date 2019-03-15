d670555284bf3cfa9232ec85fc1af36f583bbde3
Sinead Foley <smfoley2nd@gmail.com>
2019-03-15 15:30:42 -0400

    Start Up page UI button click tests

--- app/src/androidTest/java/edu/vcu/mymail/alberghinig/hackheist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/vcu/mymail/alberghinig/hackheist/ExampleInstrumentedTest.java
@@ -2,11 +2,28 @@ package edu.vcu.mymail.alberghinig.hackheist;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.support.test.espresso.Espresso;
+
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.intent.Intents.*;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -23,4 +40,43 @@ public class ExampleInstrumentedTest {
 
         assertEquals("edu.vcu.mymail.alberghinig.hackheist", appContext.getPackageName());
     }
+
+    @Rule
+    public IntentsTestRule<Welcome> intentIntentsTestRule = new IntentsTestRule<Welcome>(Welcome.class);
+
+    @Test
+    public void welcomePageHelpButton() {
+        //Check the help button on the start up screen goes to the help page
+        Espresso.onView(withId(R.id.Welcome_HelpButton)).perform(click());
+        intended(hasComponent(hasClassName(Help.class.getName())));
+    }
+
+    @Test
+    public void welcomePageLoginButton() {
+        //Check the login button on the start up screen goes to the help page
+        Espresso.onView(withId(R.id.Welcome_LoginButton)).perform(click());
+        intended(hasComponent(hasClassName(Login.class.getName())));
+    }
+
+    @Test
+    public void welcomePageSignUpButton() {
+        //Check the sign up button on the start up screen goes to the help page
+        Espresso.onView(withId(R.id.Welcome_SignupButton)).perform(click());
+        intended(hasComponent(hasClassName(SignUp.class.getName())));
+    }
+
+    @Test
+    public void welcomePageInfoButton() {
+        //Check the info button on the start up screen goes to the help page
+        Espresso.onView(withId(R.id.Welcome_InfoButton)).perform(click());
+        intended(hasComponent(hasClassName(DevInformation.class.getName())));
+    }
+
+    @Test
+    public void welcomePageForgotButton() {
+        //Check the forgot button on the start up screen goes to the help page
+        Espresso.onView(withId(R.id.Welcome_ForgotButton)).perform(scrollTo(), click());
+        intended(hasComponent(hasClassName(Forgot.class.getName())));
+    }
+
 }
