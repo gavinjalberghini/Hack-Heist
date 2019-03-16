bcbc2ecd33c4c16db094d4a2180cd449cb3f8c94
Gavin Alberghini <33029288+gavinjalberghini@users.noreply.github.com>
2019-03-16 19:31:54 -0400

    Created working leaderboard

--- app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Leaderboard.java
+++ app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Leaderboard.java
@@ -27,7 +27,7 @@ public class Leaderboard extends AppCompatActivity {
         TextView rankBox3 = findViewById(R.id.Leaderboard_Rank3TextView);
         TextView rankBox4 = findViewById(R.id.Leaderboard_Rank4TextView);
         TextView rankBox5 = findViewById(R.id.Leaderboard_Rank5TextView);
-        TextView rankBoxUser = findViewById(R.id.Leaderboard_RankUserTextView);
+        //TextView rankBoxUser = findViewById(R.id.Leaderboard_RankUserTextView);
 
 
 
@@ -36,7 +36,7 @@ public class Leaderboard extends AppCompatActivity {
         TextView userBox3 = findViewById(R.id.Leaderboard_Username3TextView);
         TextView userBox4 = findViewById(R.id.Leaderboard_Username4TextView);
         TextView userBox5 = findViewById(R.id.Leaderboard_Username5TextView);
-        TextView userBoxUser = findViewById(R.id.Leaderboard_UsernameUserTextView);
+        //TextView userBoxUser = findViewById(R.id.Leaderboard_UsernameUserTextView);
 
 
         TextView scoreBox1 = findViewById(R.id.Leaderboard_Score1TextView);
@@ -44,7 +44,7 @@ public class Leaderboard extends AppCompatActivity {
         TextView scoreBox3 = findViewById(R.id.Leaderboard_Score3TextView);
         TextView scoreBox4 = findViewById(R.id.Leaderboard_Score4TextView);
         TextView scoreBox5 = findViewById(R.id.Leaderboard_Score5TextView);
-        TextView scoreBoxUser = findViewById(R.id.Leaderboard_ScoreUserTextView);
+        //TextView scoreBoxUser = findViewById(R.id.Leaderboard_ScoreUserTextView);
 
 
         ArrayList<User> leaderboard = controller.getListOfUsers();
@@ -52,11 +52,85 @@ public class Leaderboard extends AppCompatActivity {
         Collections.sort(leaderboard, new Comparator<User>()  {
             @Override
             public int compare(User o1, User o2) {
-                String actor1 = o1.getUsername();
-                String actor2 = o2.getUsername();
-                return actor1.compareTo(actor2);
+                String userName1 = o1.getUsername();
+                String userName2 = o2.getUsername();
+                return userName1.compareTo(userName2);
             }
         });
+
+        for(int i=0; i<=5; i++){
+
+            if(leaderboard.size() > i){
+                switch(i){
+
+                    case 0:
+                        rankBox1.setText("Rank " + (i+1));
+                        userBox1.setText(leaderboard.get(i).getUsername());
+                        scoreBox1.setText("000");
+                        break;
+                    case 1:
+                        if(0 == leaderboard.get(i-1).getUsername().compareTo(leaderboard.get(i).getUsername()))
+                            rankBox2.setText(rankBox1.getText().toString());
+                        else {
+                            String nextRank = rankBox2.getText().toString();
+                            nextRank = nextRank.substring(4);
+                            nextRank = nextRank.trim();
+                            int nextRankInt = Integer.parseInt(nextRank);
+                            nextRankInt ++;
+                            nextRank = "Rank " + nextRankInt;
+                            rankBox3.setText(nextRank);
+                        }
+                        userBox2.setText(leaderboard.get(i).getUsername());
+                        scoreBox2.setText("000");
+                        break;
+                    case 2:
+                        if(0 == leaderboard.get(i-1).getUsername().compareTo(leaderboard.get(i).getUsername()))
+                            rankBox3.setText(rankBox2.getText().toString());
+                        else {
+                            String nextRank = rankBox2.getText().toString();
+                            nextRank = nextRank.substring(4);
+                            nextRank = nextRank.trim();
+                            int nextRankInt = Integer.parseInt(nextRank);
+                            nextRankInt ++;
+                            nextRank = "Rank " + nextRankInt;
+                            rankBox3.setText(nextRank);
+                        }
+                        userBox3.setText(leaderboard.get(i).getUsername());
+                        scoreBox3.setText("000");
+                        break;
+                    case 3:
+                        if(0 == leaderboard.get(i-1).getUsername().compareTo(leaderboard.get(i).getUsername()))
+                            rankBox4.setText(rankBox3.getText().toString());
+                        else {
+                            String nextRank = rankBox3.getText().toString();
+                            nextRank = nextRank.substring(4);
+                            nextRank = nextRank.trim();
+                            int nextRankInt = Integer.parseInt(nextRank);
+                            nextRankInt ++;
+                            nextRank = "Rank " + nextRankInt;
+                            rankBox4.setText(nextRank);
+                        }
+                        userBox4.setText(leaderboard.get(i).getUsername());
+                        scoreBox4.setText("000");
+                        break;
+                    case 4:
+                        if(0 == leaderboard.get(i-1).getUsername().compareTo(leaderboard.get(i).getUsername()))
+                            rankBox5.setText(rankBox4.getText().toString());
+                        else {
+                            String nextRank = rankBox4.getText().toString();
+                            nextRank = nextRank.substring(4);
+                            nextRank = nextRank.trim();
+                            int nextRankInt = Integer.parseInt(nextRank);
+                            nextRankInt ++;
+                            nextRank = "Rank " + nextRankInt;
+                            rankBox5.setText(nextRank);
+                        }
+                        userBox5.setText(leaderboard.get(i).getUsername());
+                        scoreBox5.setText("000");
+                        break;
+                }
+            }
+        }
 
 
         View.OnClickListener goBackEvent = new View.OnClickListener() {
