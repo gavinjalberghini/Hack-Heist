e221ff24949d9afe7ed4dc67b40801a5b16f2e82
Gavin Alberghini <33029288+gavinjalberghini@users.noreply.github.com>
2019-03-16 18:39:08 -0400

    Added leaderboard references

--- app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Leaderboard.java
+++ app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Leaderboard.java
@@ -1,32 +1,19 @@
 package edu.vcu.mymail.alberghinig.hackheist;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.RequestParams;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
+
 
 public class Leaderboard extends AppCompatActivity {
 
     DBController controller = new DBController(this);
-    ProgressDialog prgDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +27,36 @@ public class Leaderboard extends AppCompatActivity {
         TextView rankBox3 = findViewById(R.id.Leaderboard_Rank3TextView);
         TextView rankBox4 = findViewById(R.id.Leaderboard_Rank4TextView);
         TextView rankBox5 = findViewById(R.id.Leaderboard_Rank5TextView);
+        TextView rankBoxUser = findViewById(R.id.Leaderboard_RankUserTextView);
+
+
 
         TextView userBox1 = findViewById(R.id.Leaderboard_Username1TextView);
         TextView userBox2 = findViewById(R.id.Leaderboard_Username2TextView);
         TextView userBox3 = findViewById(R.id.Leaderboard_Username3TextView);
         TextView userBox4 = findViewById(R.id.Leaderboard_Username4TextView);
         TextView userBox5 = findViewById(R.id.Leaderboard_Username5TextView);
+        TextView userBoxUser = findViewById(R.id.Leaderboard_UsernameUserTextView);
+
 
         TextView scoreBox1 = findViewById(R.id.Leaderboard_Score1TextView);
         TextView scoreBox2 = findViewById(R.id.Leaderboard_Score2TextView);
         TextView scoreBox3 = findViewById(R.id.Leaderboard_Score3TextView);
         TextView scoreBox4 = findViewById(R.id.Leaderboard_Score4TextView);
         TextView scoreBox5 = findViewById(R.id.Leaderboard_Score5TextView);
+        TextView scoreBoxUser = findViewById(R.id.Leaderboard_ScoreUserTextView);
 
+
+        ArrayList<User> leaderboard = controller.getListOfUsers();
+
+        Collections.sort(leaderboard, new Comparator<User>()  {
+            @Override
+            public int compare(User o1, User o2) {
+                String actor1 = o1.getUsername();
+                String actor2 = o2.getUsername();
+                return actor1.compareTo(actor2);
+            }
+        });
 
 
         View.OnClickListener goBackEvent = new View.OnClickListener() {
