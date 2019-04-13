183fc42879a04027cdc44cd60f5833b482a2c619
Sinead Foley <smfoley2nd@gmail.com>
2019-04-13 17:35:39 -0400

    Added levels content, updated completion, and added drawables hh_logo and main_menu_buttons

--- app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Completion.java
+++ app/src/main/java/edu/vcu/mymail/alberghinig/hackheist/Completion.java
@@ -7,11 +7,14 @@ package edu.vcu.mymail.alberghinig.hackheist;
 
 //imports necessary libraries
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /*
  *Creates public class Completion settings extending the app compatible activity
@@ -28,7 +31,7 @@ public class Completion extends AppCompatActivity {
         setContentView(R.layout.activity_completion);
 
         //Initializes buttons
-        final Button homeButton = findViewById(R.id.Completion_HomeButton);
+        final Button emailButton = findViewById(R.id.Completion_EmailButton);
         final ImageButton backButton = findViewById(R.id.Completion_BackButton);
 
         //Sets listener for the back button to be clicked and sends the program to the main menu class
@@ -40,17 +43,44 @@ public class Completion extends AppCompatActivity {
             }
         };
 
-        //Sets listener for the menu button to be clicked and sends the program to the main menu class
-        View.OnClickListener menuScreen = new View.OnClickListener() {
+        //Sets listener for the email button to be clicked and sends the program to the email
+        View.OnClickListener emailScreen = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent I = new Intent(getApplicationContext(), MainMenu.class);
-                startActivity(I);
+                sendEmail();
             }
         };
 
         //Listens for the activity to be started, depending on the button that the user clicked
         backButton.setOnClickListener(goBackEvent);
-        homeButton.setOnClickListener(menuScreen);
+        emailButton.setOnClickListener(emailScreen);
     }
+
+    protected void sendEmail() {
+        Log.i("Send email", "");
+
+        ActiveUser user = new ActiveUser(false);
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("message/rfc822");
+        emailIntent.setPackage("com.google.android.gm");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, user.getEmail());
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Congratulations! You did it!");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Congratulations " + user.getFirstName() + "! \nYou have completed HackHeist. Now you are ready to move on to bigger and better things. We wish you the best of luck in your future endeavors.\n\n\n Sincerely,\nThe Hack Heist Team");
+//        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("@drawable/certificate.jpg"));
+
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(Completion.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
